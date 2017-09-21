c30eee7397a366a74779612db0b8a834358d5ec7
viphone <18516966845@163.com>
2017-09-21 17:28:13 +0800

    乐考无忧项目初版代码，各个项目组，请先看redeme文档

---
+++ src/test/java/testConn.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Created by Mr.W on 2017/9/21.
+ */
+@ContextConfiguration(value = "classpath:applicationContext.xml")
+public class testConn {
+    @Test
+    public void conntest() {
+        System.out.println();
+
+    }
+}
